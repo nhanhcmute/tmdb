8c0861431442aac5c57079dd31d0e35dbfe85751
FIRST_NAME LAST_NAME <19110415@student.hcmute.edu.vn>
2025-06-03 16:01:14 +0700

    new2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Footer from "./components/Layout/Footer/Footer";
 import Header from "./components/Layout/Header/Header";
 import Section from "./components/Layout/Section/Section";
 import PopularMovie from "./components/Layout/MovieAndTVCard/PopularMovie";
+import { useGlobalContext } from "./context";
+import "./main.css";
 import InfoCard from "./components/Layout/MoreInfo/InfoCard";
 
 const POPULARMOVIEAPI =
@@ -24,22 +26,54 @@ const TOPRATEDTVAPI =
   "https://api.themoviedb.org/3/tv/top_rated?api_key=700a119d738aa19bfa6867998fafed10";
 
 function App() {
+  const state = useGlobalContext();
+
   return (
     <>
       <Header className="header" />
       <Routes>
-        <Route path="/" element={<Section />} />
+        <Route exact path="/" element={<Section />} />
         {/* Movies */}
-        <Route path="/movie" element={<PopularMovie API={POPULARMOVIEAPI} />} />
-        <Route path="/movie/now-playing" element={<PopularMovie API={NOWPLAYINGAPI} />} />
-        <Route path="/movie/upcoming" element={<PopularMovie API={UPCOMINGMOVIEAPI} />} />
-        <Route path="/movie/top-rated" element={<PopularMovie API={TOPRATEDMOVIEAPI} />} />
+        <Route
+          exact
+          path="/movie"
+          element={<PopularMovie API={POPULARMOVIEAPI} />}
+        />
+        <Route
+          exact
+          path="/movie/now-playing"
+          element={<PopularMovie API={NOWPLAYINGAPI} />}
+        />
+        <Route
+          exact
+          path="/movie/upcoming"
+          element={<PopularMovie API={UPCOMINGMOVIEAPI} />}
+        />
+        <Route
+          exact
+          path="/movie/top-rated"
+          element={<PopularMovie API={TOPRATEDMOVIEAPI} />}
+        />
+        {/* End of Movies Page */}
         {/* TV Shows */}
-        <Route path="/tv" element={<PopularMovie API={POPULARTVAPI} />} />
-        <Route path="/tv/airing-today" element={<PopularMovie API={AIRINGTODAYTVAPI} />} />
-        <Route path="/tv/on-tv" element={<PopularMovie API={ONTVMOVIEAPI} />} />
-        <Route path="/tv/top-rated" element={<PopularMovie API={TOPRATEDTVAPI} />} />
-        {/* Show more info */}
+        <Route exact path="/tv" element={<PopularMovie API={POPULARTVAPI} />} />
+        <Route
+          exact
+          path="/tv/airing-today"
+          element={<PopularMovie API={AIRINGTODAYTVAPI} />}
+        />
+        <Route
+          exact
+          path="/tv/on-tv"
+          element={<PopularMovie API={ONTVMOVIEAPI} />}
+        />
+        <Route
+          exact
+          path="/tv/top-rated"
+          element={<PopularMovie API={TOPRATEDTVAPI} />}
+        />
+        {/* End of TV Shows Page */}
+        {/* show more info */}
         <Route path="/:type/:id" element={<InfoCard />} />
       </Routes>
       <Footer />
